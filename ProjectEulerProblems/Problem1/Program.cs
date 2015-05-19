0ac2e9ba84318372c0d3c5dc384d3d5555badaaf
Tairaka <tapwater16@gmail.com>
2015-05-18 20:42:33 -0600

    Added Comments for Problem1
    Added link to Problem1 and its description in comments

--- ProjectEulerProblems/Problem1/Program.cs
+++ ProjectEulerProblems/Problem1/Program.cs
@@ -8,6 +8,20 @@ namespace Problem1
 {
 	class Program
 	{
+		//**************************************************************************
+		// The following problem can be found here:
+		//	https://projecteuler.net/problem=1
+		//
+		// Problem 1
+		// Multiples of 3 and 5
+		//
+		// If we list all the natural numbers below 10 that are multiples of 3 or 5,
+		// we get 3, 5, 6 and 9. The sum of these multiples is 23.
+		//
+		// Find the sum of all the multiples of 3 or 5 below 1000.
+		//
+		//**************************************************************************
+
 		static void Main(string[] args)
 		{
 		}
