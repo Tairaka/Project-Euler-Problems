37a60a406b96e6c754dd35303beef723eadd906e
Tairaka <tapwater16@gmail.com>
2015-05-18 20:54:41 -0600

    Added solution for Problem1
    Created for loop that checked for multiples of 3 and 5. Sum each
    multiple without duplicates.

--- ProjectEulerProblems/Problem1/Program.cs
+++ ProjectEulerProblems/Problem1/Program.cs
@@ -24,6 +24,18 @@ namespace Problem1
 
 		static void Main(string[] args)
 		{
+			int sum = 0;
+
+			for (int i = 0; i < 1000; i++)
+			{
+				if((i % 3 == 0) || (i % 5 == 0))
+				{
+					sum += i;
+				}
+			}
+
+			Console.WriteLine(sum);
+			Console.Read();
 		}
 	}
 }
